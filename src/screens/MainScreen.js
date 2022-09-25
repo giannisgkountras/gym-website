feddae80d7fbae04c144965c35504440ec4cdb6e
giannisgkountras <gkountrasioannis@gmail.com>
2022-09-25 22:13:14 +0300

    footer2 on main screen

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,15 @@
 import React from "react";
 import TopNav from "../components/TopNav/TopNav";
-import Footer from "../components/Footer/Footer";
+import Footer2 from "../components/Footer2/Footer2";
 import Base from "../components/Base/Base";
-import "./MainScreen.css";
+// import "./MainScreen.css";
 
 export default function MainScreen() {
   return (
     <div className="main-screen-container">
       <TopNav />
       <Base />
-      <Footer />
+      <Footer2 />
     </div>
   );
 }
